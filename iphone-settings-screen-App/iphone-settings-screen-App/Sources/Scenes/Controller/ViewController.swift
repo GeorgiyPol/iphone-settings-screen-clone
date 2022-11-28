032a6ac91a54ad69ea4d4ac73396c151b12dec10
Georgiy <33875528+georg1856@users.noreply.github.com>
2022-11-28 17:04:43 +1000

    Initial commit

---
+++ iphone-settings-screen-App/iphone-settings-screen-App/Sources/Scenes/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iphone-settings-screen-App
+//
+//  Created by Georgiy on 28.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
